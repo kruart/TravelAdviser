a3425867bb7e5985ece9e18dd839c7db1fd01b63
weoz <weoz@ukr.net>
2016-09-25 16:52:01 +0300

    2-25_Station and composite entities

---
+++ germes-model/src/main/java/org/itsimulator/germes/app/model/entity/person/Account.java
@@ -0,0 +1,11 @@
+package org.itsimulator.germes.app.model.entity.person;
+
+import org.itsimulator.germes.app.model.entity.base.AbstractEntity;
+
+/**
+ * Entity that encapsulates user of the application
+ * @author admin
+ *
+ */
+public class Account extends AbstractEntity{
+}
