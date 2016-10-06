b5188d739fdfb2f28eefb35fb25c5ccfc057bdc6
weoz <weoz@ukr.net>
2016-10-06 18:07:08 +0300

    13-60 Added logging libraries(Slf4j, Log4j) and utility libraries (commons-lang, guava)

--- germes-service/src/main/java/org/itsimulator/germes/app/service/transform/impl/SimpleDTOTransformer.java
+++ germes-service/src/main/java/org/itsimulator/germes/app/service/transform/impl/SimpleDTOTransformer.java
@@ -1,10 +1,14 @@
 package org.itsimulator.germes.app.service.transform.impl;
 
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.itsimulator.germes.app.infra.util.Checks;
 import org.itsimulator.germes.app.infra.util.ReflectionUtil;
 import org.itsimulator.germes.app.model.entity.base.AbstractEntity;
 import org.itsimulator.germes.app.rest.dto.base.BaseDTO;
 import org.itsimulator.germes.app.service.transform.Transformer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Default transformation engine that uses reflection to transform objects
@@ -12,6 +16,7 @@ import org.itsimulator.germes.app.service.transform.Transformer;
  *
  */
 public class SimpleDTOTransformer implements Transformer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleDTOTransformer.class);
 
     @Override
     public <T extends AbstractEntity, P extends BaseDTO<T>> P transform(final T entity, final Class<P> clz) {
@@ -21,6 +26,10 @@ public class SimpleDTOTransformer implements Transformer {
         //Now just copy all the similar fields
         ReflectionUtil.copyFields(entity, dto, ReflectionUtil.findSimilarFields(entity.getClass(), clz));
         dto.transform(entity);
+
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("SimpleDTOTransformer.transform: {} DTO object", ReflectionToStringBuilder.toString(dto, ToStringStyle.SHORT_PREFIX_STYLE));
+        }
 
         return dto;
     }
@@ -33,6 +42,10 @@ public class SimpleDTOTransformer implements Transformer {
 
         ReflectionUtil.copyFields(dto, entity, ReflectionUtil.findSimilarFields(dto.getClass(), clz));
         dto.untransform(entity);
+
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("SimpleDTOTransformer.untransform: {} entity", ReflectionToStringBuilder.toString(entity, ToStringStyle.SHORT_PREFIX_STYLE));
+        }
 
         return entity;
     }
