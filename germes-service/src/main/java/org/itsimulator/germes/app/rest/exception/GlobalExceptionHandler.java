96d4243ab9a0be1d05771f5a1ee6392a52da9db8
weoz <weoz@ukr.net>
2016-10-16 14:07:03 +0300

    18-65 GlobalExceptionHandler

---
+++ germes-service/src/main/java/org/itsimulator/germes/app/rest/exception/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package org.itsimulator.germes.app.rest.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * Global Jersey handler that caches any Exception-based errors
+ *
+ * @author admin
+ *
+ */
+@Provider
+public class GlobalExceptionHandler implements ExceptionMapper<Exception> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    private final Response SERVER_ERROR;
+
+    public GlobalExceptionHandler() {
+        SERVER_ERROR = Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+    }
+
+    @Override
+    public Response toResponse(Exception e) {
+        LOGGER.error(e.getMessage(), e);
+
+        return SERVER_ERROR;
+    }
+}
