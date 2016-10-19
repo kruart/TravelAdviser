fb52c2f1df7756913501010975c66e5b4351f1e2
weoz <weoz@ukr.net>
2016-10-19 22:00:41 +0300

    21-71 added Mockito and PowerMock dependencies

--- germes-service/src/test/java/org/itsimulator/germes/app/service/transform/impl/CacheFieldProviderTest.java
+++ germes-service/src/test/java/org/itsimulator/germes/app/service/transform/impl/CacheFieldProviderTest.java
@@ -1,11 +1,18 @@
 package org.itsimulator.germes.app.service.transform.impl;
 
+import org.itsimulator.germes.app.infra.util.ReflectionUtil;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 /**
  * Verifies functionality of the {@link SimpleDTOTransformer} unit
@@ -13,6 +20,8 @@ import static org.junit.Assert.*;
  * @author admin
  *
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(ReflectionUtil.class)
 public class CacheFieldProviderTest {
     private FieldProvider provider;
 
@@ -31,6 +40,19 @@ public class CacheFieldProviderTest {
     @Test
     public void testGetFieldNamesCachedSuccess() {
         List<String> fields = provider.getFieldNames(Source.class, Destination.class);
+        List<String> fields2 = provider.getFieldNames(Source.class, Destination.class);
+        assertFalse(fields.isEmpty());
+        assertEquals(fields, fields2);
+    }
+
+    @Test
+    public void testGetFieldNamesAreCached() {
+        List<String> fields = provider.getFieldNames(Source.class, Destination.class);
+
+        PowerMockito.mockStatic(ReflectionUtil.class);
+        when(ReflectionUtil.findSimilarFields(Source.class, Destination.class)).thenReturn(Collections.emptyList());
+
+        assertTrue(ReflectionUtil.findSimilarFields(Source.class, Destination.class).isEmpty());
         List<String> fields2 = provider.getFieldNames(Source.class, Destination.class);
         assertFalse(fields.isEmpty());
         assertEquals(fields, fields2);
