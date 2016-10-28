90b92d003737fd0098f1268d7281abf4a54accce
weoz <weoz@ukr.net>
2016-10-28 14:55:48 +0300

    39-99 Testing the embedded DBs(H2, HSQLDB, DERBY, SQLite)

---
+++ germes-service/src/main/java/org/itsimulator/germes/app/config/A.java
@@ -0,0 +1,8 @@
+package org.itsimulator.germes.app.config;
+
+public class A {
+	public static void main(String[] args) throws ClassNotFoundException {
+		Class.forName("org.hibernate.dialect.SQLiteDialect");
+	}
+
+}
