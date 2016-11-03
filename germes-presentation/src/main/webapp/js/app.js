7cd25f43b1d30bcd58775da906ebf2a28050f510
weoz <weoz@ukr.net>
2016-11-03 12:52:43 +0200

    55-119 angular service(cityService)

--- germes-presentation/src/main/webapp/js/app.js
+++ germes-presentation/src/main/webapp/js/app.js
@@ -1,3 +1,22 @@
 'use strict';
 
 var app = angular.module("app", []);
+
+app.factory('cityService', [function () {
+    var cities = [{
+        'name' : 'Odessa',
+        'district' : '',
+        'region' : 'Odessa'
+    },
+        {
+            'name' : 'Izmail',
+            'district' : 'Izmail',
+            'region' : 'Odessa'
+        }];
+
+    return {
+        'getCities' : function () {
+            return cities
+        }
+    }
+}]);
--- germes-presentation/src/main/webapp/js/controllers.js
+++ germes-presentation/src/main/webapp/js/controllers.js
@@ -1,11 +1,3 @@
-app.controller('CityCtrl', ['$scope', function ($scope) {
-    $scope.cities = [{
-        'name': 'Odessa',
-        'distinct': '',
-        'region': 'Odesssa'
-    },{
-        'name' : 'Izmail',
-        'district' : 'Izmail',
-        'region' : 'Odessa'
-    }]
+app.controller('CityCtrl', ['$scope', 'cityService', function ($scope, cityService) {
+    $scope.cities = cityService.getCities();
 }]);
