05c046933e65e84fcdfe4f94f1182f3012e13362
weoz <weoz@ukr.net>
2016-11-11 13:59:57 +0200

    66-130 using angular-translate

--- germes-presentation/src/main/webapp/js/app.js
+++ germes-presentation/src/main/webapp/js/app.js
@@ -1,7 +1,32 @@
 'use strict';
 
-var app = angular.module("app", ['ngResource']);
+var app = angular.module('app', [ 'ngResource', 'pascalprecht.translate' ]);
 
-app.factory('cityService', ['$resource', function ($resource) {
+app.factory('cityService', [ '$resource', function($resource) {
     return $resource('/api/cities');
-}]);
+} ]);
+
+app.config(function($translateProvider) {
+    $translateProvider.translations('en', {
+        TITLE : 'Germes project',
+        CITIES : 'Cities',
+        CITY_NAME: 'Name',
+        CITY_DISTRICT : 'District',
+        CITY_REGION : 'Region'
+    });
+    $translateProvider.translations('ru', {
+        TITLE : 'Проект Гермес',
+        CITIES : 'Населенные пункты',
+        CITY_NAME: 'Название',
+        CITY_DISTRICT : 'Район',
+        CITY_REGION : 'Область'
+    });
+    $translateProvider.translations('uk', {
+        TITLE : 'Проект Гермес',
+        CITIES : 'Населені пункти',
+        CITY_NAME: 'Назва',
+        CITY_DISTRICT : 'Район',
+        CITY_REGION : 'Область'
+    });
+    $translateProvider.preferredLanguage('en');
+});
--- germes-presentation/src/main/webapp/test/project.conf.js
+++ germes-presentation/src/main/webapp/test/project.conf.js
@@ -17,6 +17,7 @@ module.exports = function(config) {
     files: [ "../bower_components/angular/angular.js",
       "../bower_components/angular-mocks/angular-mocks.js",
       "../bower_components/angular-resource/angular-resource.js",
+      "../bower_components/angular-translate/angular-translate.js",
       "../js/*.js",
       "unit/*.js"
     ],
